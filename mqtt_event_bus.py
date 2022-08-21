27db96327788ee7c9d65efa3eb41a99626e03300
Michdo93 <michaeldoerflinger93@gmail.com>
2022-08-21 10:21:55 +0200

    Update mqtt_event_bus.py

--- mqtt_event_bus.py
+++ mqtt_event_bus.py
@@ -1,146 +1,216 @@
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import string
+import random
 import logging
-import time
+import concurrent.futures
 from typing import List
 import HABApp
 from HABApp import Parameter
 from HABApp.core.events import ValueChangeEvent, ValueUpdateEvent
-from HABApp.mqtt.items import MqttItem
 from HABApp.openhab.events import ItemCommandEvent, ItemStateEvent
 from HABApp.openhab.items import OpenhabItem
 
 log = logging.getLogger('MQTTEventBus')
 
-# These are the configuration values that will be used to setup the MqttEventBus
 log_state = Parameter('mqtt_event_bus', 'log_state', default_value=True).value
-onInit = Parameter('mqtt_event_bus', 'onInit', default_value=True).value
-statePublishTopic = Parameter(
-    'mqtt_event_bus', 'statePublishTopic', default_value='').value
-commandPublishTopic = Parameter(
-    'mqtt_event_bus', 'commandPublishTopic', default_value='').value
-stateSubscribeTopic = Parameter(
-    'mqtt_event_bus', 'stateSubscribeTopic', default_value='').value
-commandSubscribeTopic = Parameter(
-    'mqtt_event_bus', 'commandSubscribeTopic', default_value='').value
 
 
 class MqttEventBus(HABApp.Rule):
     def __init__(self):
         super().__init__()
 
-        #onInit = None
+        self.onInit = Parameter(
+            'mqtt_event_bus', 'onInit', default_value=True).value
+        self.brokerTransport = Parameter(
+            'mqtt_event_bus', 'brokerTransport', default_value="tcp").value
+        self.brokerIP = Parameter(
+            'mqtt_event_bus', 'brokerIP', default_value="192.168.0.5").value
+        self.brokerPort = Parameter(
+            'mqtt_event_bus', 'brokerPort', default_value=1883).value
+        self.clientId = Parameter(
+            'mqtt_event_bus', 'clientId', default_value="OpenHABVM").value
+        self.tlsPath = Parameter(
+            'mqtt_event_bus', 'tlsPath', default_value="").value
+        self.tlsVersion = Parameter(
+            'mqtt_event_bus', 'tlsVersion', default_value="").value
+        self.brokerUser = Parameter(
+            'mqtt_event_bus', 'brokerUser', default_value="").value
+        self.brokerPWD = Parameter(
+            'mqtt_event_bus', 'brokerPWD', default_value="").value
+        self.brokerQOS = Parameter(
+            'mqtt_event_bus', 'brokerQOS', default_value=0).value
+        self.retain = Parameter(
+            'mqtt_event_bus', 'retain', default_value="").value
+        self.brokerAsync = Parameter(
+            'mqtt_event_bus', 'brokerAsync', default_value="").value
+        self.statePublishTopic = Parameter(
+            'mqtt_event_bus', 'statePublishTopic', default_value="/messages/states/${item}").value
+        self.commandPublishTopic = Parameter(
+            'mqtt_event_bus', 'commandPublishTopic', default_value="").value
+        self.stateSubscribeTopic = Parameter(
+            'mqtt_event_bus', 'stateSubscribeTopic', default_value="").value
+        self.commandSubscribeTopic = Parameter(
+            'mqtt_event_bus', 'commandSubscribeTopic', default_value="/messages/commands/${item}").value
 
-        # while onInit is None:
-        #    time.sleep(1)
+        rand = "".join(random.choice(string.ascii_uppercase + string.digits)
+                       for _ in range(3))
 
-        # if onInit:
-        #    self._do_init()
-        #    onInit = False
+        if self.brokerQOS not in range(0, 3):
+            self.brokerQOS = 0
 
-        for item in self.get_items(type=OpenhabItem):
-            #self.openhab.post_update(item.name, item.get_value())
-            #self.openhab.send_command(item.name, "")
-            if statePublishTopic != '':
-                item.listen_event(self.on_item_state, ItemStateEvent)
-                #item.listen_event(self.on_item_state, ValueUpdateEvent)
+        self.auth = None
+        if self.brokerUser is not None:
+            if self.brokerPWD is not None:
+                self.auth = {'username': self.brokerUser,
+                             'password': self.brokerPWD}
+            else:
+                self.auth = {'username': self.brokerUser,
+                             'password': ""}
+        else:
+            self.auth = None
 
-            if commandPublishTopic != '':
-                item.listen_event(self.on_item_command, ItemCommandEvent)
-                #item.listen_event(self.on_item_command, ValueUpdateEvent)
+        if self.brokerUser == "" or self.brokerUser == None:
+            self.auth = None
 
-            if commandSubscribeTopic != '':
-                topic_command = commandSubscribeTopic.replace(
-                    "${item}", str(item.name))
+        self.broker_tls = None
+        if self.brokerPort != 1883:
+            if self.tlsPath is not None or self.tlsPath != "":
+                if self.tlsVersion is not None or self.tlsVersion != "":
+                    self.broker_tls = (self.tlsPath, self.tlsVersion)
+                else:
+                    self.broker_tls = self.tlsPath
+            else:
+                self.broker_tls = None
 
-                mqtt_item_command = MqttItem.get_create_item(
-                    f'{topic_command}')
-                mqtt_item_command.listen_event(
-                    self.on_mqtt_command, ValueUpdateEvent)
-                mqtt_item_command.listen_event(
-                    self.on_mqtt_command, ValueChangeEvent)
+        self.client = mqtt.Client(client_id=self.clientId[:20]+rand, clean_session=True,
+                                  userdata=None, protocol=mqtt.MQTTv311, transport=self.brokerTransport)
 
-            if stateSubscribeTopic != '':
-                topic_state = stateSubscribeTopic.replace(
-                    "${item}", str(item.name))
+        if self.broker_tls is not None:
+            self.client.tls_set(self.broker_tls)
 
-                mqtt_item_state = MqttItem.get_create_item(f'{topic_state}')
-                mqtt_item_state.listen_event(
-                    self.on_mqtt_state, ValueUpdateEvent)
-                mqtt_item_state.listen_event(
-                    self.on_mqtt_state, ValueChangeEvent)
+        if self.retain == True:
+            self.retain = True
+        else:
+            self.retain = False
 
-    def _do_init(self):
-        all_items = self.get_items(type=OpenhabItem)
+        if self.brokerAsync == True:
+            self.brokerAsync = True
+        else:
+            self.brokerAsync = False
 
-        for item in all_items:
-            # publish current item state in chunks
-            chunk_size = 50
-            chunk_nr = 0
-            chunk = all_items[chunk_nr *
-                              chunk_size:(chunk_nr + 1) * chunk_size]
-            while chunk:
-                #self.run.at(chunk_nr + 1, self._publish_current_state, chunk)
-                self.run.at(chunk_nr + 1, self._publish_current_command, chunk)
-                chunk_nr += 1
-                chunk = all_items[chunk_nr *
-                                  chunk_size:(chunk_nr + 1) * chunk_size]
+        self.topics = []
 
-    def _publish_current_state(self, items: List[OpenhabItem]):
-        for item in items:
-            topic = statePublishTopic.replace("${item}", item.name)
-            # if statePublishTopic != '':
-            self.mqtt.publish(topic, str(item.value), retain=True)
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            for item in self.get_items(type=OpenhabItem):
+                if self.statePublishTopic:
+                    if self.onInit:
+                        executor.submit(self.on_init_item(
+                            item.name, item.get_value(), self.statePublishTopic), item.name)
+                    executor.submit(item.listen_event(
+                        self.on_item_state, ItemStateEvent), item.name)
 
-    def _publish_current_command(self, items: List[OpenhabItem]):
-        for item in items:
-            topic = commandPublishTopic.replace("${item}", event.name)
-            # if commandPublishTopic != '':
-            self.mqtt.publish(topic, "")
+                if self.commandPublishTopic:
+                    executor.submit(item.listen_event(
+                        self.on_item_command, ItemCommandEvent), item.name)
 
-    def on_mqtt_command(self, event):
-        assert isinstance(event, ValueUpdateEvent)
+                if self.commandSubscribeTopic:
+                    tpc = self.commandSubscribeTopic.replace(
+                        "${item}", str(item.name))
+                    executor.submit(self.topics.append(
+                        (tpc, self.brokerQOS)), item.name)
 
-        name = event.name
-        itemPosition = commandSubscribeTopic.split("${item}")[0].count("/")
+                if self.stateSubscribeTopic:
+                    tpc = self.stateSubscribeTopic.replace(
+                        "${item}", str(item.name))
+                    executor.submit(self.topics.append(
+                        (tpc, self.brokerQOS)), item.name)
 
-        item = name.split("/")[itemPosition]
-        cmd = event.value
+            self.client.on_connect = self.on_connect
+            self.client.on_message = self.on_message
+            self.client.username_pw_set(self.auth)
+            self.client.connect(self.brokerIP, self.brokerPort)
+            self.client.loop_forever()
 
-        log.info(f'Subscribed MQTT topic {event.name} with {cmd}')
-        log.info(f'{item} predicted to become {cmd}')
+    def on_connect(self, client, userdata, flags, rc):
+        self.client.subscribe(self.topics)
 
-        self.openhab.send_command(item, cmd)
+    def on_message(self, client, userdata, msg):
+        topic = msg.topic
+        stateOrCommand = str(msg.payload.decode("utf-8"))
+
+        log.info(
+            f'Subscribed MQTT topic {topic} with {stateOrCommand}')
+
+        if self.stateSubscribeTopic:
+            event_type = 'state'
+            itemPosition = self.stateSubscribeTopic.split("${item}")[
+                0].count("/")
+
+        if self.commandSubscribeTopic:
+            event_type = 'command'
+            itemPosition = self.commandSubscribeTopic.split("${item}")[
+                0].count("/")
+
+        item_name = topic.split("/")[itemPosition]
+
+        if self.openhab.item_exists(item_name) == True:
+            if event_type == "command":
+                self.openhab.send_command(
+                    item_name, stateOrCommand)
+
+                log.info(
+                    f'Item {item_name} received command {stateOrCommand}')
+            elif event_type == "state":
+                self.openhab.post_update(item_name, stateOrCommand)
+                log.info(
+                    f'Item {item_name} received update {stateOrCommand}')
+
+    def on_init_item(self, item, value, topicString):
+        topicString = topicString.replace("${item}", item)
+        topic = f'{topicString}'
+
+        log.info(f'Published MQTT topic {topic} with {value}')
+
+        rand = "".join(random.choice(string.ascii_uppercase + string.digits)
+                       for _ in range(3))
+
+        publish.single(topic=topic, payload=str(value), qos=self.brokerQOS, retain=self.retain, hostname=self.brokerIP,
+                       port=self.brokerPort, client_id=self.clientId[:20]+rand, keepalive=60, will=None,
+                       auth=self.auth, tls=self.broker_tls,
+                       protocol=mqtt.MQTTv311, transport=self.brokerTransport)
 
     def on_item_state(self, event: ItemStateEvent):
-        topicString = statePublishTopic.replace(
+        topicString = self.statePublishTopic.replace(
             "${item}", event.name)
         topic = f'{topicString}'
         value = event.value
 
         log.info(f'Published  MQTT topic {topic} with {value}')
 
-        self.mqtt.publish(topic, str(value), retain=True)
+        rand = "".join(random.choice(string.ascii_uppercase + string.digits)
+                       for _ in range(3))
+
+        publish.single(topic=topic, payload=str(value), qos=self.brokerQOS, retain=self.retain, hostname=self.brokerIP,
+                       port=self.brokerPort, client_id=self.clientId[:20]+rand, keepalive=60, will=None,
+                       auth=self.auth, tls=self.broker_tls,
+                       protocol=mqtt.MQTTv311, transport=self.brokerTransport)
 
     def on_item_command(self, event: ItemCommandEvent):
-        topicString = commandPublishTopic.replace(
+        topicString = self.commandPublishTopic.replace(
             "${item}", event.name)
         topic = f'{topicString}'
         value = event.value
 
         log.info(f'Published  MQTT topic {topic} with {value}')
 
-        self.mqtt.publish(topic, str(value))
+        rand = "".join(random.choice(string.ascii_uppercase + string.digits)
+                       for _ in range(3))
 
-    def on_mqtt_state(self, event):
-        assert isinstance(event, ValueUpdateEvent)
-
-        name = event.name
-        itemPosition = stateSubscribeTopic.split("${item}")[0].count("/")
-
-        item = name.split("/")[itemPosition]
-        state = event.value
-
-        log.info(f'Subscribed MQTT topic {name} with {state}')
-        self.openhab.post_update(item, state)
+        publish.single(topic=topic, payload=str(value), qos=self.brokerQOS, retain=self.retain, hostname=self.brokerIP,
+                       port=self.brokerPort, client_id=self.clientId[:20]+rand, keepalive=60, will=None,
+                       auth=self.auth, tls=self.broker_tls,
+                       protocol=mqtt.MQTTv311, transport=self.brokerTransport)
 
 
 class LogItemStateRule(HABApp.Rule):
@@ -149,8 +219,10 @@ class LogItemStateRule(HABApp.Rule):
     def __init__(self):
         super().__init__()
 
-        for item in self.get_items(type=OpenhabItem):
-            item.listen_event(self.on_item_change, ValueChangeEvent)
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            for item in self.get_items(type=OpenhabItem):
+                executor.submit(item.listen_event(
+                    self.on_item_change, ValueChangeEvent), item.name)
 
     def on_item_change(self, event):
         assert isinstance(event, ValueChangeEvent)
